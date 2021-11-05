4d450b5fd89c16e3b1dc7366e55f332fe3ba484a
Edgarmym <88387095+Edgarmym@users.noreply.github.com>
2021-11-05 09:37:50 -0500

    Update conn_tipo_servicio.php

--- Buggy/conn_tipo_servicio.php
+++ Buggy/conn_tipo_servicio.php
@@ -9,7 +9,7 @@
         //CONSULTAR DATOS EN BD
         $u = "postgres";
         $p = "postgres";
-        $conn = new PDO('pgsql:host=localhost;port=5432;dbname=bd_general', $u, $p);
+        $conn = new PDO('pgsql:host=192.168.100.3;port=5432;dbname=bd_general', $u, $p);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //EJECUTA CONSULTA
         if ($idtipo == 0) {
